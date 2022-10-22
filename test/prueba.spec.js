66852f8a2b2a2bea9b38d6926bec808701be5e78
Jorge Garnica <jorgegarba@gmail.com>
2022-10-22 16:33:59 -0500

    adding one test

--- test/prueba.spec.js
+++ test/prueba.spec.js
@@ -13,4 +13,8 @@ describe('Sum', () => {
 	it('should handle decimals', () => {
 		assert.equal(sum(3.5, 7.1), 10.6);
 	});
+	
+	it('it should not handle strings', () => {
+		assert.equal(sum('3', '5'), '35');
+	});
 });
